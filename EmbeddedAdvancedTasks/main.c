97ba00bc6205785ad556410a06cb164849e0d3c0
Demiana Younes <110495430+DemianaYounes@users.noreply.github.com>
2024-10-31 05:00:12 +0300

    Update main.c

--- EmbeddedAdvancedTasks/main.c
+++ EmbeddedAdvancedTasks/main.c
@@ -5,20 +5,36 @@
  * Author : Demiana
  */ 
 
+#define F_CPU 8000000
+#include <util/delay.h>
 #include "StdTypes.h"
 
-#include "DIO_Interface.h"
 #include "TrafficLight_Interface.h"
+#include "EventBased_Interface.h"
 
 
+#define TRAFFIC_LIGHT    0
+#define EVENT_BASED      2
+#define APPLICATION      EVENT_BASED
 
+#if APPLICATION == TRAFFIC_LIGHT
 int main(void)
 {
     /* Replace with your application code */
-	TracfficLight_voidInit();
+	TrafficLight_voidInit();
     while (1) 
     {
-		TracfficLight_voidRunnable();
+		TrafficLight_voidRunnable();
     }
 }
 
+#elif APPLICATION==EVENT_BASED
+int main()
+{
+	EventBased_Init();
+	while(1)
+	{
+		EventBased_Runnable();
+	}
+}
+#endif
